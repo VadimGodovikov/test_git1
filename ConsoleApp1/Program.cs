e00d3c66e55a2b55934bfe3911749d65dc2fca22
VadimGodovikov <vadimka.332@mail.ru>
2024-04-19 00:20:31 +0300

    Создание класса напитка, класса сортировки и основная суть прогарммы

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -9,28 +10,71 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        class Soda
+        class Drink
         {
-            public void show_my_drink(string ing)
+            public string Name;
+            public double litr;
+
+            public void Print()
             {
-                if( ing == null || ing == "" )
-                {
-                    Console.WriteLine("Обычная газировка");
-                }
-                else
-                {
-                    Console.WriteLine($"Газировка + {ing}");
-                }
+                Console.WriteLine("Название: " + Name);
+                Console.WriteLine("Объём в литрах: " +  litr);
+            }
+            public void WriteToFile(StreamWriter sw)
+            {
+                sw.WriteLine("Название: " + Name);
+                sw.WriteLine("Объём в литрах: " + litr);
+                sw.WriteLine();
+            }
+        }
+
+        class DrinkComparer : IComparer<Drink> //  сортировка по алфавиту
+        {
+            public int Compare(Drink x, Drink y)
+            {
+                return string.Compare(x.Name, y.Name, StringComparison.Ordinal);
             }
         }
         static void Main(string[] args)
         {
-            Soda drink1 = new Soda();
-            Soda drink2 = new Soda();
-            Soda drink3 = new Soda();
-            drink1.show_my_drink("Малина");
-            drink2.show_my_drink("");
-            drink3.show_my_drink(null);
+            w1:
+            Console.Write("Введите количество напитков, которые хотите записать: ");
+            int size = int.Parse(Console.ReadLine());
+            if(size <= 0)
+            {
+                Console.WriteLine("Вы ввели некорректное число, попробуйте ещё раз");
+                goto w1;
+            }
+
+            Drink[] drinks = new Drink[size];
+            for (int i = 0; i < size;  i++)
+            {
+                drinks[i] = new Drink();
+                Console.Write("Введите название " + (i+1) + " напитка: ");
+                drinks[i].Name = Console.ReadLine();
+                Console.Write("Введите объём в литрах: ");
+                drinks[i].litr = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+            }
+
+            // Сортировка списка напитков по алфавиту
+            Array.Sort(drinks, new DrinkComparer());
+
+            // Вывод отсортированных напитков
+            foreach (var drink in drinks)
+            {
+                drink.Print();
+            }
+
+            using (StreamWriter sw = new StreamWriter("drinks.txt")) // запись в файл
+            {
+                foreach (var drink in drinks)
+                {
+                    drink.WriteToFile(sw);
+                }
+            }
+
         }
     }
 }
